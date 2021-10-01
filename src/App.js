69b00ff177ae38c57b03a06e35a1cf5d26956569
denisBriceag <denisbriceag21@gmail.com>
2021-10-01 20:02:27 +0300

    NEw material ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Greetings from './Greetings';
 
 function App() {
   return (
--- src/Greetings.js
+++ src/Greetings.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+import { ButtonGroup } from '@material-ui/core';
+import { Checkbox } from '@material-ui/core';
+import './Greetings.css';
+
+const style = { "color": "red", "font-size": 20, "background-color": "green" };
 
 const Greetings = () => {
     return (
         <div>
-            <h1>Hello world!</h1>
-            <h1>My name is Denis</h1>
+            <ButtonGroup orientation="vertical">
+                <Button variant="contained" color="secondary" size="large" onClick={() => { alert('Hello!') }}>
+                    Hello!
+                </Button>
+                <Button className="red" onClick={() => { alert('Salut!') }}>Salut</Button>
+                <Button style={style} onClick={() => { alert('Bonjour!') }}>Bonjour</Button>
+                <Button onClick={() => { alert('Привет!') }}>Привет</Button>
+                <Button onClick={() => { alert('aaaaa!') }}>ааааа</Button>
+            </ButtonGroup>
+            <Checkbox defaultChecked />
+            <Checkbox />
+            <Checkbox />
         </div>
     )
 }
